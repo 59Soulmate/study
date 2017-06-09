24ff59fa0bc3361ac80a3edd803094b07b95ade3
lyvee <570184201@qq.com>
2017-06-09 16:33:21 +0800

    新建项目

--- gitTest/src/main/java/com/sd/entity/User.java
+++ gitTest/src/main/java/com/sd/entity/User.java
@@ -4,4 +4,31 @@ package com.sd.entity;
  * Created by sd-kf on 2017/6/9.
  */
 public class User {
+    private Integer id;
+    private String name;
+    private String pass;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
